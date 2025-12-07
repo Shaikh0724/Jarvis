0da089dbc818568682a81f087905d135204eda6b
Shaikh0724 <shaikh.wahab71@gmail.com>
2025-12-07 21:26:31 +0500

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music={
+    "wolf":"https://youtu.be/8of5w7RgcTc?si=1NNotwD31GoRhz89",
+    "sky":"https://youtu.be/erWHu1Jc5oU?si=A8H89RslOUMlAP4N",
+    "gharnari":"https://youtu.be/qaWf0Nw8ogw?si=gW3rZYKKNsQRPbGU",
+    "poem":"https://youtu.be/6rRRAVSilss?si=T483dVAgjEDG1fMe"
+}
